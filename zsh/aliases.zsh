6ebd0bec576e6226a5941cfff1fa7bc5b6b92436
Mykyta Khylko <khylko98@gmail.com>
2026-01-06 17:26:00 +0200

    add password generator to zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,3 +7,9 @@ alias ls='ls --color=auto --group-directories-first'
 alias dir='dir --color=auto'
 alias grep='grep --color=auto'
 alias ip='ip --color=auto'
+
+# Password generator
+pwgen() {
+  local len=${1:-20}
+  LC_ALL=C tr -dc 'A-Za-z0-9@#%^&*()_+=-{}[]:;<>,.?/' < /dev/urandom | head -c "$len" | xargs echo
+}
